dd77e51d9b03fd4761b37c5fa270630ccccb005f
Dinara Nurtdinova <dinara.nurtdinova@icloud.com>
2025-05-09 13:39:01 +0300

    удалила ненужный комментарий

--- blocks/main/creative-process/modal-window.js
+++ blocks/main/creative-process/modal-window.js
@@ -1,22 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const openBtn = document.querySelector('.open-modal');         // Кнопка
-//     const modal = document.querySelector('.modal');                // Модальное окно
-//     const closeBtn = document.querySelector('.modal__close');      // Крестик
-//     const overlay = document.querySelector('.modal__overlay');     // Фон
-  
-//     openBtn.addEventListener('click', () => {
-//       modal.classList.add('active'); // Показываем окно
-//     });
-  
-//     closeBtn.addEventListener('click', () => {
-//       modal.classList.remove('active'); // Закрываем окно
-//     });
-  
-//     overlay.addEventListener('click', () => {
-//       modal.classList.remove('active'); // Закрываем по клику на фон
-//     });
-// });
-
 document.addEventListener('DOMContentLoaded', function () {
     const openBtn = document.querySelector('.open-modal');
     const modal = document.querySelector('.modal');                
